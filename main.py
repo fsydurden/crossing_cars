7e9f14c888597442d3dd80b9fe1f49c0ed0d6eff
karthik <jakustove@gmail.com>
2025-07-18 23:44:35 +0530

    project finished

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ player = Player()
 screen.listen()
 screen.onkey(player.move, "Up")
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 
 game_is_on = True
@@ -25,11 +26,14 @@ while game_is_on:
     #detect collision with car
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
+            scoreboard.game_over()
             game_is_on = False
+
 
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_uo()
+        scoreboard.increase_level()
 
 
 screen.exitonclick()
